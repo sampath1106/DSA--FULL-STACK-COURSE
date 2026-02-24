88ad04aea7962602f4668b6ef30c7a7bec6701db
sampath1106 <murududdiajay@gmail.com>
2026-02-24 13:37:50 +0530

    Add files via upload

---
+++ #constructor.py
@@ -0,0 +1,7 @@
+#constructor
+class sbi():
+    def__init__(self,money):
+       self.money=money
+       print("money added to your account:",money)
+cash=int(input("enter a  amount:"))
+cash=sbi(cash)
