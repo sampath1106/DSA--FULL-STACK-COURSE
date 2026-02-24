275823d85091021680fd99dde94310532453b637
sampath1106 <murududdiajay@gmail.com>
2026-02-24 13:40:32 +0530

    Add files via upload

---
+++ indrect recussion.py
@@ -0,0 +1,12 @@
+#indrect recussion
+def is_even(n):
+    if n==0:
+       return True
+    return is_odd(n-1)
+def is_odd(n):
+    if n==0:
+      return False
+    return is_even(n-1)
+n=int(input("enter a value:"))
+print("Even",is_even(n))
+
