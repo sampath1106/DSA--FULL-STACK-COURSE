b498f953267b96d01b869d04f3db4aadad2dafb4
sampath1106 <murududdiajay@gmail.com>
2026-03-14 11:04:50 +0530

    Add files via upload

---
+++ array +hashmap (reapeating  elementts).py
@@ -0,0 +1,11 @@
+#array +hashmap find gthe fist repeating element in array dta structure input:5 3 4 3 2 2
+
+arr=list(map(int,input("element:").split()))
+freq={}
+for  num in arr:
+    if num in freq:
+        print("first repeating element:",num)
+        break
+    freq[num]=1
+else:
+    print("no repeating elements:")
