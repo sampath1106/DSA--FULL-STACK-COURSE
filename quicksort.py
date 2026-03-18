738abad3c907a8b0548bd38a2718925ea0e10472
sampath1106 <murududdiajay@gmail.com>
2026-03-18 16:08:36 +0530

    Add files via upload

---
+++ quicksort.py
@@ -0,0 +1,17 @@
+def quicksort(arr,low,high):
+    if low<high:
+        pi=partition(arr,low,high)
+        quicksort(arr,low,pi-1)
+        quicksort(arr,pi+1,high)
+def partition(arr,low,high):
+    piviot=arr[high]
+    i=low-1
+    for j in range(low,high):
+        if arr[j]<piviot:
+            i+=1
+            arr[i],arr[j]=arr[j],arr[i]
+    arr[i+1],arr[high]=arr[high],arr[i+1]
+    return i+1
+arr=list(map(int,input("element:").split()))
+quicksort(arr,0,len(arr)-1)
+print(arr)
